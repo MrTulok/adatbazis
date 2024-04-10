dc448e13a44703375536c90e6609eb153f650f2e
MrTulok <tulokvendel@gmail.com>
2024-04-10 13:10:14 +0200

    gyak7

--- gyak.sql
+++ gyak.sql
@@ -1,15 +1,21 @@
 SELECT IIF(GROUPING_ID(v.USERNEV) = 1,'Végösszeg',v.USERNEV),
-    count(distinct szh.SZALLAS_ID)
+    count(distinct (szh.SZALLAS_ID))
 FROM Vendeg v LEFT JOIN Foglalas f ON f.UGYFEL_FK = v.USERNEV
-    JOIN Szoba szo ON szo.SZOBA_ID = f.SZOBA_FK
-    JOIN Szallashely szh ON szo.SZALLAS_FK = szh.SZALLAS_ID
+    left JOIN Szoba szo ON szo.SZOBA_ID = f.SZOBA_FK
+    left JOIN Szallashely szh ON szo.SZALLAS_FK = szh.SZALLAS_ID
 GROUP BY ROLLUP (v.USERNEV)
 
-SELECT DISTINCT v.USERNEV, v.NEV
-FROM Vendeg v JOIN Foglalas f ON f.UGYFEL_FK = v.USERNEV
-WHERE v.USERNEV NOT IN
+select *,
+    DATEDIFF(YEAR,SZUL_DAT,GETDATE()),
+    AVG(DATEDIFF(YEAR,SZUL_DAT,GETDATE())) over(order by SZUL_DAT rows between 2 PRECEDING and 2 FOLLOWING)
+from Vendeg
+
+select v.USERNEV,v.NEV
+from Vendeg v JOIN Foglalas f ON v.USERNEV = f.UGYFEL_FK
+WHERE not EXISTS
 (
-    SELECT f2.UGYFEL_FK
-    FROM Foglalas f2
-    WHERE 
+    select *
+    from foglalas f2
+    where v.USERNEV = f2.UGYFEL_FK AND f2.FELNOTT_SZAM + f2.GYERMEK_SZAM <> 2
 )
+
